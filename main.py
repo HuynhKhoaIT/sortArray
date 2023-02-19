373592f2498331eef0428544c323918e80fd673b
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-02-19 21:31:01 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# nhập mảng từ người dùng
+arr = input("Nhập mảng, cách nhau bằng dấu phẩy: ")
+arr = arr.split(",")
+arr = [int(x) for x in arr]
+
+# sắp xếp mảng
+arr.sort()
+
+# in kết quả
+print("Mảng sau khi sắp xếp: ", arr)
